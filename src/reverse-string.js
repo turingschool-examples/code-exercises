e8fbb374503fdd168f2e334c093e0f1b9c5b450e
Paul Nguyen <paul.t.nguyen7@gmail.com>
2016-10-18 10:04:38 -0600

    reverse string without an array

--- src/reverse-string.js
+++ src/reverse-string.js
@@ -1,4 +1,13 @@
 module.exports = (string) => {
-  var reverseString= string.split('').reverse().join('');
-  return reverseString;
+  // THIS EXAMPLE USES AN ARRAY
+  // var reverseString = string.split('').reverse().join('');
+  // return reverseString;
+
+  // THIS EXAMPLE TAKES A STRING AND RETURNS A STRING, W/OUT AN ARRAY
+  let result = '';
+  // START AT THE END OF THE STRING LENGTH AND START FROM THE END
+  for(let i=string.length-1; i>=0; i--) {
+    result += string[i];
+  }
+  return result;
 };
